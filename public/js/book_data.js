ebf25410b2901907a09b6dda71f6ad307cc61e4a
finasilvasantiste <fina.santiste@gmail.com>
2018-07-07 13:38:37 -0700

    Moved bubblegraph js to its own file, adjusted svg position

---
+++ public/js/book_data.js
@@ -0,0 +1,84 @@
+
+// Based loosely from this D3 bubble graph https://bl.ocks.org/mbostock/3063269
+// And this Forced directed diagram https://bl.ocks.org/mbostock/3062045
+let data = [{
+			cat: 'Contemporary', name: 'A Little Life', value: 100,
+			icon: 'img/alittlelife.png',
+			
+		}, {
+			cat: 'Sci-Fi', name: 'Annihilation', value: 100,
+			icon: 'img/annihilation.png',
+			
+		}, {
+			cat: 'Sci-Fi', name: 'Authority', value: 10,
+			icon: 'img/authority.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'The Cruel Prince', value: 60,
+			icon: 'img/cruelprince.png',
+			
+		}, {
+			cat: 'Contemporary', name: 'Mrs. Fletcher', value: 100,
+			icon: 'img/fletcher.png',
+			
+		}, {
+			cat: 'Thriller', name: 'Last Winter We Parted', value: 100,
+			icon: 'img/lastwinter.png',
+			
+		}, {
+			cat: 'Non-Fiction', name: 'A Uterus is a Feauture not a Bug', value: 30,
+			icon: 'img/notabug.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'An Enchantment of Ravens', value: 60,
+			icon: 'img/ravens.png',
+			
+		}, {
+			cat: 'Sci-Fi', name: 'Red Clocks', value: 10,
+			icon: 'img/redclocks.png',
+				
+		}, {
+			cat: 'Fantasy', name: 'A Court of Wings and Ruin', value: 60,
+			icon: 'img/acowar.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'Dreams of Gods and Monsters', value: 100,
+			icon: 'img/dreams.png',
+			
+		}, {
+			cat: 'Sci-Fi', name: 'Kindred', value: 100,
+			icon: 'img/kindred.png',
+				
+		}, {
+			cat: 'Contemporary', name: 'Big Little Lies', value: 30,
+			icon: 'img/lies.png',
+			
+		}, {
+			cat: 'Contemporary', name: 'Me Before You', value: 30,
+			icon: 'img/moyes.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'The Ocean at the End of the Lane', value: 60,
+			icon: 'img/ocean.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'The Red Queen', value: 30,
+			icon: 'img/red.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'Days of Blood and Starlight', value: 100,
+			icon: 'img/starlight.png',
+			
+		}, {
+			cat: 'Contemporary', name: 'Stone Mattress', value: 30,
+			icon: 'img/stone.png',
+			
+		}, {
+			cat: 'Fantasy', name: 'Interview with a Vampire', value: 30,
+			icon: 'img/vampire.png',
+			
+		}, {
+			cat: 'Sci-Fi', name: 'Vaterland', value: 100,
+			icon: 'img/vaterland.png',
+			
+		}];
